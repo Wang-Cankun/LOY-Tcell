953722faa400171c67ab4eb2ddc0753c78a435f6
Cankun Wang <flykun0620@gmail.com>
2023-08-22 22:18:44 -0400

    add training using Symphony update

--- TcellAtlas_Symphony_mapping/build_atlas_symphony.R
+++ TcellAtlas_Symphony_mapping/build_atlas_symphony.R
@@ -5,43 +5,40 @@ library(qs)
 library(tidyverse)
 library(Seurat)
 library(SeuratDisk)
+library(symphony)
+library(singlecellmethods)
+
+# remotes::install_github("immunogenomics/singlecellmethods")
 
 setwd('/Users/wang.13246/Documents/Project/loy/model_train')
 set.seed(42)
-
-
 
 
 # Atlas
 sample_name <- "GC_sample.rds"
 combined <- read_rds(sample_name)
 DefaultAssay(combined) <- "RNA"
-
-combined2 <- UpdateSeuratObject(combined) # GC_sample.rds is an old version Seurat object, need to run UpdateSeuratObject
-combined2@assays$RNA@scale.data <- matrix() # scaled data is not needed
-#combined2@assays$RNA@data <- combined2@assays$RNA@counts
-SaveH5Seurat(combined2,
-             filename = paste0(sample_name, ".h5Seurat"),
-             overwrite = T)
-Convert(paste0(sample_name, ".h5Seurat"),
-        dest = "h5ad",
-        overwrite = T)
+combined <- UpdateSeuratObject(combined) # GC_sample.rds is an old version Seurat object, need to run UpdateSeuratObject
 
 
+# Run symphony
+ref_exp_full <- GetAssayData(combined, slot = "data")
+ref_metadata = combined@meta.data
 
-# Query data
-sample_name <- "Seurat.merge.T.excluded.qs"
-combined <- qs::qread(sample_name)
-DefaultAssay(combined) <- "RNA"
+reference = symphony::buildReference(
+  ref_exp_full,                   # reference expression (genes by cells)
+  ref_metadata,              # reference metadata (cells x attributes)
+  vars = c('sample'),         # variable(s) to integrate over
+  K = 100,                   # number of Harmony soft clusters
+  verbose = TRUE,            # display verbose output
+  do_umap = TRUE,            # run UMAP and save UMAP model to file
+  do_normalize = FALSE,      # perform log(CP10k) normalization on reference expression
+  vargenes_method = 'vst',   # variable gene selection method: 'vst' or 'mvp'
+  vargenes_groups = 'sample', # metadata column specifying groups for variable gene selection within each group
+  topn = 2000,               # number of variable genes (per group)
+  theta = 2,                 # Harmony parameter(s) for diversity term
+  d = 20,                    # number of dimensions for PCA
+  save_uwot_path = './reference_Tcell_atlas', # file path to save uwot UMAP model
+  additional_genes = NULL    # vector of any additional genes to force include
+)
 
-combined2 <- UpdateSeuratObject(combined)
-combined2@assays$RNA@scale.data <- matrix() # scaled data is not needed
-combined2@assays$RNA@data <- matrix() # normalized data is not needed
-#combined2@assays$RNA@data <- combined2@assays$RNA@counts
-combined2@meta.data <- combined2@meta.data[, 1:10] # Some metadata are all NAs, causing errors while loading in Scanpy
-SaveH5Seurat(combined2,
-             filename = paste0(sample_name, ".h5Seurat"),
-             overwrite = T)
-Convert(paste0(sample_name, ".h5Seurat"),
-        dest = "h5ad",
-        overwrite = T)
