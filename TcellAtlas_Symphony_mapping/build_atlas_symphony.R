c60b3769ce11895e5f506b79e950f26b892db064
Cankun Wang <flykun0620@gmail.com>
2023-08-22 22:17:20 -0400

    add training using Symphony

---
+++ TcellAtlas_Symphony_mapping/build_atlas_symphony.R
@@ -0,0 +1,47 @@
+
+# Convert Seurat object to h5ad
+library(here)
+library(qs)
+library(tidyverse)
+library(Seurat)
+library(SeuratDisk)
+
+setwd('/Users/wang.13246/Documents/Project/loy/model_train')
+set.seed(42)
+
+
+
+
+# Atlas
+sample_name <- "GC_sample.rds"
+combined <- read_rds(sample_name)
+DefaultAssay(combined) <- "RNA"
+
+combined2 <- UpdateSeuratObject(combined) # GC_sample.rds is an old version Seurat object, need to run UpdateSeuratObject
+combined2@assays$RNA@scale.data <- matrix() # scaled data is not needed
+#combined2@assays$RNA@data <- combined2@assays$RNA@counts
+SaveH5Seurat(combined2,
+             filename = paste0(sample_name, ".h5Seurat"),
+             overwrite = T)
+Convert(paste0(sample_name, ".h5Seurat"),
+        dest = "h5ad",
+        overwrite = T)
+
+
+
+# Query data
+sample_name <- "Seurat.merge.T.excluded.qs"
+combined <- qs::qread(sample_name)
+DefaultAssay(combined) <- "RNA"
+
+combined2 <- UpdateSeuratObject(combined)
+combined2@assays$RNA@scale.data <- matrix() # scaled data is not needed
+combined2@assays$RNA@data <- matrix() # normalized data is not needed
+#combined2@assays$RNA@data <- combined2@assays$RNA@counts
+combined2@meta.data <- combined2@meta.data[, 1:10] # Some metadata are all NAs, causing errors while loading in Scanpy
+SaveH5Seurat(combined2,
+             filename = paste0(sample_name, ".h5Seurat"),
+             overwrite = T)
+Convert(paste0(sample_name, ".h5Seurat"),
+        dest = "h5ad",
+        overwrite = T)
